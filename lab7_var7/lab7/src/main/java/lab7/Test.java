ebc45ae1239babda3b3cf065c447e538c6c4ab25
alekseykrazhev <kr69@mail.ru>
2021-10-25 18:24:58 +0300

    lab 7

---
+++ lab7_var7/lab7/src/main/java/lab7/Test.java
@@ -0,0 +1,56 @@
+package lab7;
+
+public class Test {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("+++     UTILITY   BILL    +++\n");
+
+            //  Creating .dat file and writing some objects from Input.txt
+            FileSupport datFile = new FileSupport("UtilityBill.dat");
+            datFile.deleteFile();
+            datFile.appendToFileFromTXTFile("Input.txt");
+            datFile.printFile();
+
+            //  MAP DEMONSTRATION
+            MapUtility billMap = new MapUtility(datFile);
+            billMap.showMap();
+
+
+            //Testing readByKey
+            System.out.println("*** Reading Bill by key: 13");
+            System.out.println(billMap.readByKey(13));
+
+            //Testing removeByKey
+            billMap.removeByKey(3);
+            System.out.println("\n*** Remove Bill by key:  3");
+            billMap.showMap();
+
+            datFile.printFile();
+
+            //OutputAscending
+            billMap.showObjectsAscending();
+            //OutputDescending
+            billMap.showObjectsDescending();
+
+            //bigger than key 13
+            billMap.showObjectBiggerThanKey(13);
+            //bigger than key 33
+            billMap.showObjectBiggerThanKey(33);
+            //bigger than key 50
+            billMap.showObjectBiggerThanKey(50);
+
+            //Smaller than 10
+            billMap.showObjectSmallerThanKey(10);
+            //Smaller than 7
+            billMap.showObjectSmallerThanKey(7);
+
+            System.out.println("\n\nThe programme worked correctly!");
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+
+    }
+}
+
