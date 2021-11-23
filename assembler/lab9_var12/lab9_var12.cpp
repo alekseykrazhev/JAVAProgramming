d50ebade41eb020a1d632f92875a132a095a77d1
alekseykrazhev <lkr37@mail.ru>
2021-11-23 17:04:29 +0300

    assembler lab1

---
+++ assembler/lab9_var12/lab9_var12.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number:\n";
+	int N;
+    std::cin >> N;
+	int count = 0;
+	int no = N * 10;
+	int ten = 10, one = 1, res = 0, z = 0.1;
+    _asm {
+		mov eax, N
+		mov ebx, ten
+		xor ecx, ecx
+		_loop_1 :
+			xor edx, edx
+			div ebx
+			cmp eax, 0
+			je _Mark_1
+			add ecx, 2
+			loop _loop_1
+		_Mark_1 :
+		mov eax, N
+		xor ebx, ebx
+		add ebx, 1
+		add ecx, 2
+		_loop_2 :
+			imul ebx, 10
+			loop _loop_2
+		imul eax, 10
+		add eax, ebx
+		add eax, 1
+		mov res, eax
+    }
+    std::cout << res;
+}
--- lab9_var12/demo/src/main/java/com/example/App.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public final class App {
-    private App() {
-    }
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
